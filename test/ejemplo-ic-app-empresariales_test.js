6df8b9ecd53abb2486870506e7f9ad637395bcad
johnba <johnbairon.londo45@gmail.com>
2015-11-21 10:19:44 -0500

    dojo integracion continua

---
+++ test/ejemplo-ic-app-empresariales_test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var ejemploIcAppEmpresariales = require('../');
+var assert = require('should');
+
+describe('ejemploIcAppEmpresariales', function () {
+
+  it('should be awesome', function () {
+    ejemploIcAppEmpresariales().should.equal('awesome');
+  });
+
+});
